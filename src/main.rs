cf0ca9e1f102c99ee930ba649c6b0bb569030f95
shu-kita <shusei3316@yahoo.co.jp>
2024-08-24 10:14:47 +0900

    add get command line options operation

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,28 @@ use engine::match_line;
 use cli::Args;
 
 fn main() {
-    let args: Args = Args::parse();
-    let pattern = args.pattern;
+    let mut args: Args = Args::parse();
+    let patterns = match args.get_patterns(){
+        Ok(pattern_list) => pattern_list,
+        Err(e) => {
+            eprintln!("{e}");
+            return
+        },
+    };
+    println!("{:?}", patterns);
+
+    let files = match args.get_files() {
+        Ok(file_list) => file_list,
+        Err(e) => {
+            eprintln!("{e}");
+            return
+        }
+    };
+    println!("{:?}", files);
+
+    let is_count = args.count;
+    let is_ignore_case = args.ignore_case;
+    let is_invert_match = args.invert_match;
 
     // println!("{}", match_line("ab(c|d)", "abc").unwrap());
     // println!("{}", match_line("ab(c|d)", "xabcdefg").unwrap());
