18546c5807c52b29a9a3c93d84689ce6c177402d
shu-kita <shusei3316@yahoo.co.jp>
2024-08-28 16:49:29 +0900

    不要な改行を削除

--- src/engine/codegen.rs
+++ src/engine/codegen.rs
@@ -258,9 +258,7 @@ impl Generator {
 pub fn get_code(ast: &AST) -> Result<Vec<Instruction>, CodeGenError> {
     let mut generator = Generator::default();
     match generator.gen_code(ast) {
-        Ok(()) => {
-            Ok(generator.instructions)
-        },
+        Ok(()) => Ok(generator.instructions),
         Err(e) => Err(e)
     }
 }
