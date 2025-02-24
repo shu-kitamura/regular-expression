9b99e116a9f56f74e711f1d5d405e0b78e694424
shu-kitamura <shusei3316@yahoo.co.jp>
2025-02-24 22:23:04 +0900

    add comment

--- src/engine/instruction.rs
+++ src/engine/instruction.rs
@@ -3,19 +3,20 @@
 
 use std::fmt::{self, Display};
 
-/// Instruction型
+/// コード生成時に使用する命令(Instruction)を表す型
 #[derive(Debug, PartialEq)]
 pub enum Instruction {
-    Char(Char),
-    Match,
-    Jump(usize),
-    Split(usize, usize),
+    Char(Char),           // 文字列をマッチする命令
+    Match,                // マッチング成功を示す命令
+    Jump(usize),          // 指定した命令アドレスにジャンプする命令
+    Split(usize, usize),  // 指定した2つの命令アドレスに分岐する命令
 }
 
+/// 文字を表す型
 #[derive(Debug, PartialEq)]
 pub enum Char {
-    Literal(char),
-    Any,
+    Literal(char), // 指定された文字（'a', 'b'など）に対応する
+    Any,           // 任意の文字('.')に対応する
 }
 
 impl Display for Instruction {
