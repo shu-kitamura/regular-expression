19cce77b9a902bde2fae8ad9f84a9b29688cbd1c
shu-kitamura <shusei3316@yahoo.co.jp>
2025-05-11 03:13:49 +0900

    fix #14 add test for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,59 @@ pub fn pattern_match(
         is_invert_match,
     )
 }
+
+// ----- テストコード -----
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pattern_match_true() {
+        // マッチが成功するケース
+        let pattern = "abc";
+        let line = "abcdef";
+        let is_ignore_case = false;
+        let is_invert_match = false;
+
+        let result = pattern_match(pattern, line, is_ignore_case, is_invert_match);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), true);
+    }
+
+    #[test]
+    fn test_pattern_match_false() {
+        // マッチが失敗するケース
+        let pattern = "xyz";
+        let line = "abcdef";
+        let is_ignore_case = false;
+        let is_invert_match = false;
+        let result = pattern_match(pattern, line, is_ignore_case, is_invert_match);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), false);
+    }
+
+    #[test]
+    fn test_pattern_match_is_ignore_case() {
+        // 大小文字を区別しないケース
+        let pattern = "abc";
+        let line = "ABCDEF";
+        let is_ignore_case = true;
+        let is_invert_match = false;
+        let result = pattern_match(pattern, line, is_ignore_case, is_invert_match);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), true);
+    }
+
+    #[test]
+    fn test_pattern_match_is_invert_match() {
+        // マッチング結果を逆にするケース
+        let pattern = "abc";
+        let line = "abcdef";
+        let is_ignore_case = false;
+        let is_invert_match = true;
+        let result = pattern_match(pattern, line, is_ignore_case, is_invert_match);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), false);
+    }
+}
